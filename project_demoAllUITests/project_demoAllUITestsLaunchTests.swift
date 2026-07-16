930c9e588c3b2c68ca8b8e9b6d38a5435cd6568a
xingbin deng <dengxingbin.dev@gmail.com>
2026-07-16 09:31:07 +0800

    Initial Commit

---
+++ project_demoAllUITests/project_demoAllUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  project_demoAllUITestsLaunchTests.swift
+//  project_demoAllUITests
+//
+//  Created by Dengxingbin on 2026/7/16.
+//
+
+import XCTest
+
+final class project_demoAllUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
